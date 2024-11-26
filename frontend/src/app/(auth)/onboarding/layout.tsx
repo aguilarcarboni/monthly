5ad798fc66dd30f902aac4a21a961059bf1d8679
Andres Aguilar <aguilarcarboni@gmail.com>
2024-11-25 22:17:11 -0600

    Update onboarding

--- frontend/src/app/(auth)/onboarding/layout.tsx
+++ frontend/src/app/(auth)/onboarding/layout.tsx
@@ -13,7 +13,7 @@ export default function Layout({
 }>) {
 
   return (
-    <div className="flex flex-col scrollbar-hide justify-center items-center h-full w-full scroll-smooth">
+    <div className="flex flex-col justify-center items-center">
       {children}
     </div>
   );
